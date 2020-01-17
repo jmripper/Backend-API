94dc0e3673e4fa1fa43bae76fcf59d09548a2bb6
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-17 12:10:39 -0500

    installed body-parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const routes = require('./lib/routes/routes')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 
 app.use('/', routes);
 

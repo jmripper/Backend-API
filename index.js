7bea273f5a86074fb93929f9db7f28a5b45b44b0
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-19 16:35:21 -0500

    added more route options to controller file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+//const cors = require("cors")
 const app = express()
 const routes = require('./lib/routes/routes')
 const bodyParser = require('body-parser')
+
+//app.use(cors());
 
 app.use(bodyParser.json());
 

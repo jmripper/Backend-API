f42e5133e69d14d0b99b03818d19d80309dd278c
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-19 21:31:10 -0500

    setup heroku create

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ app.use(bodyParser.json());
 console.log("listening")
 app.listen(5000);
 
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
+
 app.use('/', routes);

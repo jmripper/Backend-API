290c0dec01d97302d16d676f02a63251b299b65b
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-21 15:34:57 -0500

    installed cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-//const cors = require("cors")
+const cors = require("cors")
 const app = express()
 const routes = require('./lib/routes/routes')
 const bodyParser = require('body-parser')
 
-//app.use(cors());
+app.use(cors());
 
 app.use(bodyParser.json());
 

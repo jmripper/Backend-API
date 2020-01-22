f17aa6e88104d9bc333b36e60db7b9b6264f3002
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-21 21:59:55 -0500

    res.header to allow fetch

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const cors = require("cors")
 const app = express()
 const routes = require('./lib/routes/routes')
 const bodyParser = require('body-parser')
+const cors = require("cors")
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 

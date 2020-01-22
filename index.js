7aae157f3a442c40a63de0c21dcad9b333f44295
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-22 12:56:30 -0500

    added back in urlencoder

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors());
-// app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {

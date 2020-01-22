e3c6200ab7d3ceb8c9e4b0cae6664c6f6f27f8c7
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-22 11:55:09 -0500

    worked on update to model and delete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express()
 const routes = require('./lib/routes/routes')
 const bodyParser = require('body-parser')
 const cors = require("cors")
+const app = express()
 
 app.use(cors());
 
@@ -14,13 +14,11 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-console.log("listening")
-app.listen(5000);
+//use routes file
+app.use('/', routes);
 
 app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
-
-app.use('/', routes);
